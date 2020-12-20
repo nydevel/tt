d0e9781ba73568c39eb10ea45b47e22ad88e8ad1
Nick Yasinskiy <1@shfu.ru>
2020-12-20 19:53:10 +0500

    Test added

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use tt;
 
+const MESSAGE_NO_ARGUMENTS: &str = "No arguments given";
+
 // Initial point
 // There we parse command line arguments and call modiles.
 fn main() {
-    let command = std::env::args().nth(1).expect("No arguments given");
-    
+    let command = std::env::args().nth(1).expect(MESSAGE_NO_ARGUMENTS);
+
     tt::init();
 
     if command == "start" {
-        println!("Timer started!");
+        println!("Timer starte!");
+    }
+}
+
+//==========================Tests==========================
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "No arguments given")]
+    fn test_main() {
+        main();
     }
 }
