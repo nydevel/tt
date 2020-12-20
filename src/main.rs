9f0cf684bb75d4d92701136cfb6d2bfb05f4cdd2
Nick Yasinskiy <1@shfu.ru>
2020-12-20 19:43:50 +0500

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use tt;
 
+// Initial point
+// There we parse command line arguments and call modiles.
 fn main() {
+    let command = std::env::args().nth(1).expect("No arguments given");
+    
     tt::init();
+
+    if command == "start" {
+        println!("Timer started!");
+    }
 }
